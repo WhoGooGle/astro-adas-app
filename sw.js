5526250a6ded5d784357afc90dfccf09c5d21082
WhoGooGle <44841905+WhoGooGle@users.noreply.github.com>
2026-03-05 21:26:22 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,27 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("astro-adas-v1").then((cache) =>
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/what-is-adas.html",
+        "/why-adas-exists.html",
+        "/when-calibration-needed.html",
+        "/sensor-locations.html",
+        "/myths-vs-facts.html",
+        "/vehicle-brands.html",
+        "/astro-process.html",
+        "/quick-check.html",
+        "/vehicle-checker.html",
+        "/lead-form.html",
+        "/manifest.json"
+      ])
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
